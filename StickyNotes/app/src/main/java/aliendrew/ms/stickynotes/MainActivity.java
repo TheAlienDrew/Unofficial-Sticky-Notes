a12ee86886c9b42d9c4d342ce43395d1655d6151
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2019-09-17 06:11:25 -0700

    Toggle theme / Refresh changes
    * Removed toggle button in favor of using volume keys to change theme (doubles as a reload button per theme too)
    * Altered theme/function of failed internet connection prompt
    * Made loading screen bigger and defined unofficial intentions
    * Updated some graphics

--- StickyNotes/app/src/main/java/aliendrew/ms/stickynotes/MainActivity.java
+++ StickyNotes/app/src/main/java/aliendrew/ms/stickynotes/MainActivity.java
@@ -2,100 +2,102 @@ package aliendrew.ms.stickynotes;
 
 // basics
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
-// webview stuff
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.net.Uri;
-// no internet connection detection
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.graphics.Color;
 // used to inject JS files
 import java.io.InputStream;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import android.util.Base64;
-// toggle button stuff
-import android.widget.ToggleButton;
-import android.widget.CompoundButton;
+// no internet connection detection
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.WindowManager.LayoutParams;
+// toggle theme requirements
+import android.view.KeyEvent;
 import android.view.View;
 import android.content.Intent;
 import android.content.SharedPreferences;
-// splash screen stuff
-import android.graphics.Color;
-// webview flicker fix
+// webView stuff
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.net.Uri;
+// webView flicker fix
 import android.webkit.JavascriptInterface;
 import android.annotation.SuppressLint;
 import android.os.Handler;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // for the toggle button
+    // Use the chosen theme
     private static final String PREFS_NAME = "prefs";
     private static final String PREF_DARK_THEME = "dark_theme";
-    private ToggleButton toggle;
+    private boolean useDarkTheme = false;
 
-    private NoSuggestionsWebView webview;
+    private NoSuggestionsWebView webView;
+    private WebView splashView;
 
     @SuppressLint({"SetJavaScriptEnabled", "AddJavascriptInterface"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // Use the chosen theme
-        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        final boolean useDarkTheme = preferences.getBoolean(PREF_DARK_THEME, false);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toggle = (ToggleButton) findViewById(R.id.toggleTheme);
-        toggle.setChecked(useDarkTheme);
-        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                toggleTheme(isChecked);
-            }
-        });
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        useDarkTheme = preferences.getBoolean(PREF_DARK_THEME, false);
 
-        webview = (NoSuggestionsWebView) findViewById(R.id.webView);
+        webView = findViewById(R.id.webView);
 
         // splash loading
-        WebView splashview = (WebView) findViewById(R.id.splashView);
-        splashview.setVerticalScrollBarEnabled(false);
-        splashview.setHorizontalScrollBarEnabled(false);
-        splashview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        splashView = findViewById(R.id.splashView);
+        splashView.setVerticalScrollBarEnabled(false);
+        splashView.setHorizontalScrollBarEnabled(false);
+        splashView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
         if (useDarkTheme) {
-            splashview.setBackgroundColor(Color.BLACK);
-            splashview.loadUrl("file:///android_asset/html/loading-dark.html");
-            webview.setBackgroundColor(Color.BLACK);
-        } else
-            splashview.loadUrl("file:///android_asset/html/loading-light.html");
+            splashView.setBackgroundColor(Color.BLACK);
+            splashView.loadUrl("file:///android_asset/html/loading-dark.html");
+            webView.setBackgroundColor(Color.BLACK);
+        } else {
+            splashView.setBackgroundColor(Color.WHITE);
+            splashView.loadUrl("file:///android_asset/html/loading-light.html");
+            webView.setBackgroundColor(Color.WHITE);
+        }
 
-        webview.setWebViewClient(new WebViewClient() {
+        webView.setWebViewClient(new WebViewClient() {
 
             // INTERNET DETECTION block
-            public void onReceivedError(WebView webView, int errorCode, String description, String failingUrl) {
-                try {
-                    webView.stopLoading();
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                if (view.canGoBack()) {
+                    view.goBack();
                 }
 
-                if (webView.canGoBack()) {
-                    webView.goBack();
-                }
-
-                webView.loadUrl("about:blank");
-                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                AlertDialog alertDialog;
+                if (useDarkTheme)
+                    alertDialog = new AlertDialog.Builder(MainActivity.this, AlertDialog.THEME_DEVICE_DEFAULT_DARK).create();
+                else
+                    alertDialog = new AlertDialog.Builder(MainActivity.this, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT).create();
+                alertDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                alertDialog.getWindow().clearFlags(LayoutParams.FLAG_DIM_BEHIND);
+                alertDialog.getWindow().getAttributes().verticalMargin = 0.3F;
                 alertDialog.setTitle("Error");
                 alertDialog.setMessage("Check your internet connection and try again.");
                 alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Try Again", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
+                        webView.reload();
+                    }
+                });
+                alertDialog.setOnCancelListener(new DialogInterface.OnCancelListener(){
+                    @Override
+                    public void onCancel(DialogInterface dialogInterface) {
                         finish();
-                        startActivity(getIntent());
                     }
                 });
 
                 alertDialog.show();
-                super.onReceivedError(webView, errorCode, description, failingUrl);
+                webView.setVisibility(View.GONE);
+                super.onReceivedError(view, errorCode, description, failingUrl);
             }
 
             // LINKS OPEN AS EXTERNAL block
@@ -108,15 +110,16 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 } else if (// open in Webview
                         url.startsWith("https://www.onenote.com/stickynotes")
-                        || url.startsWith("https://www.onenote.com/common1pauth/signin?redirectUrl=https%3A%2F%2Fwww.onenote.com%2Fstickynotes")
-                        || url.startsWith("https://login.windows.net/common/oauth2/authorize")
-                        || url.startsWith("https://login.microsoftonline.com/common/oauth2/authorize")
-                        || url.startsWith("https://www.onenote.com/common1pauth/exchangecode")
-                        || url.startsWith("https://login.live.com/oauth20_authorize.srf")
-                        || url.startsWith("https://login.live.com/ppsecure/post.srf")
-                        || url.startsWith("https://www.onenote.com/common1pauth/msaimplicitauthcallback?redirectUrl=https%3a%2f%2fwww.onenote.com%2fstickynotes")
-                        || url.startsWith("https://www.onenote.com/common1pauth/signout?redirectUrl=https%3A%2F%2Fwww.onenote.com%2Fcommon1pauth%2Fsignin%3FredirectUrl%3Dhttps%253A%252F%252Fwww.onenote.com%252Fstickynotes")
+                                || url.startsWith("https://www.onenote.com/common1pauth/signin?redirectUrl=https%3A%2F%2Fwww.onenote.com%2Fstickynotes")
+                                || url.startsWith("https://login.windows.net/common/oauth2/authorize")
+                                || url.startsWith("https://login.microsoftonline.com/common/oauth2/authorize")
+                                || url.startsWith("https://www.onenote.com/common1pauth/exchangecode")
+                                || url.startsWith("https://login.live.com/oauth20_authorize.srf")
+                                || url.startsWith("https://login.live.com/ppsecure/post.srf")
+                                || url.startsWith("https://www.onenote.com/common1pauth/msaimplicitauthcallback?redirectUrl=https%3a%2f%2fwww.onenote.com%2fstickynotes")
+                                || url.startsWith("https://www.onenote.com/common1pauth/signout?redirectUrl=https%3A%2F%2Fwww.onenote.com%2Fcommon1pauth%2Fsignin%3FredirectUrl%3Dhttps%253A%252F%252Fwww.onenote.com%252Fstickynotes")
                 ){
+                    webView.setVisibility(View.GONE);
                     return false;
                 }
                 // open rest of URLS in default browser
@@ -126,16 +129,17 @@ public class MainActivity extends AppCompatActivity {
             }
 
             // THEME MOD block
+
             @Override
             public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(webview, url);
+                super.onPageFinished(webView, url);
 
                 if (useDarkTheme)
-                    injectScriptFile(webview, "js/dark_theme.js");
+                    injectScriptFile(webView, "js/dark_theme.js");
                 else
-                    injectScriptFile(webview, "js/light_theme.js");
+                    injectScriptFile(webView, "js/light_theme.js");
 
-                webview.loadUrl("javascript: window.CallToAnAndroidFunction.setVisible()");
+                webView.loadUrl("javascript: window.CallToAnAndroidFunction.setVisible()");
             }
 
             private void injectScriptFile(WebView view, String scriptFile) {
@@ -143,6 +147,7 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     input = getAssets().open(scriptFile);
                     byte[] buffer = new byte[input.available()];
+                    //noinspection ResultOfMethodCallIgnored
                     input.read(buffer);
                     input.close();
 
@@ -157,40 +162,58 @@ public class MainActivity extends AppCompatActivity {
                             "parent.appendChild(script)" +
                             "})()");
                 } catch (IOException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
             }
         });
 
         // enables site to work
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
         // visual fixes
-        webview.setVerticalScrollBarEnabled(false);
-        webview.setHorizontalScrollBarEnabled(false);
-        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
         // fixes keyboard suggestions/auto correct on some keyboards
-        webview.getSettings().setSaveFormData(false);
-        webview.clearFormData();
-        webview.clearFormData();
+        webView.getSettings().setSaveFormData(false);
+        webView.clearFormData();
+        webView.clearFormData();
 
-        webview.addJavascriptInterface(new myJavaScriptInterface(), "CallToAnAndroidFunction");
-        webview.loadUrl("https://www.onenote.com/stickynotes");
-
+        webView.addJavascriptInterface(new myJavaScriptInterface(), "CallToAnAndroidFunction");
+        webView.loadUrl("https://www.onenote.com/stickynotes");
     }
 
+    // volume button control to change theme/reload page
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_VOLUME_UP)){
+            toggleTheme(false);
+        } else if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)){
+            toggleTheme(true);
+        }
+        return true;
+    }
+
+    // toggles theme in shared preferences
     private void toggleTheme(boolean darkTheme) {
         SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
         editor.putBoolean(PREF_DARK_THEME, darkTheme);
         editor.apply();
 
-        Intent intent = getIntent();
-        finish();
-
-        startActivity(intent);
+        if (darkTheme) {
+            splashView.setBackgroundColor(Color.BLACK);
+            splashView.loadUrl("file:///android_asset/html/loading-dark.html");
+            webView.setBackgroundColor(Color.BLACK);
+        } else {
+            splashView.setBackgroundColor(Color.WHITE);
+            splashView.loadUrl("file:///android_asset/html/loading-light.html");
+            webView.setBackgroundColor(Color.WHITE);
+        }
+        useDarkTheme = darkTheme;
+        webView.reload();
     }
 
+    // used to enable webView after full load of theme
     public class myJavaScriptInterface {
         @JavascriptInterface
         public void setVisible(){
@@ -200,10 +223,9 @@ public class MainActivity extends AppCompatActivity {
                     new Handler().postDelayed(new Runnable() {
                         @Override
                         public void run() {
-                            toggle.setVisibility(View.VISIBLE);
-                            webview.setVisibility(View.VISIBLE);
+                            webView.setVisibility(View.VISIBLE);
                         }
-                    }, 500);
+                    }, 800);
                 }
             });
         }
