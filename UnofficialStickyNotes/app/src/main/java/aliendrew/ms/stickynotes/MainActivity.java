ec49eeae332e6b2fcfdce86de24f30735b3bf763
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-03-24 20:16:40 -0700

    App should now be fullscreen
    The app should now take advantage of the full screen size, by auto minimizing the system bars

--- UnofficialStickyNotes/app/src/main/java/aliendrew/ms/stickynotes/MainActivity.java
+++ UnofficialStickyNotes/app/src/main/java/aliendrew/ms/stickynotes/MainActivity.java
@@ -47,6 +47,33 @@ public class MainActivity extends AppCompatActivity {
 
     private NoSuggestionsWebView webStickies;
 
+    // allow the app to hide the bars top and bottom to use the full screen
+    // * taken from the android dev docs
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY // altered so it doesn't stay on screen
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
